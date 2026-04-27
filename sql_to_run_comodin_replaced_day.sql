8ab2a4bdb8f05745ca5c1d7a5501833a0440d5c4
Diego Patzan <diegopatzan24@gmail.com>
2026-04-27 09:19:56 -0600

    [REMOVE] drop

--- sql_to_run_comodin_replaced_day.sql
+++
@@ -1,2 +0,0 @@
--- 2. Agregamos la columna 'replaced_day' a la tabla 'check_ins' (si no existe) para guardar que dia de la semana reponen
-ALTER TABLE public.check_ins ADD COLUMN IF NOT EXISTS replaced_day INT;
